880b4f612ad4dbf731dbc4ae138a5ee16239d8c2
Adam Gnatowski <adamg91v2@gmail.com>
2019-12-03 11:53:13 +0100

    exercise done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-let John = [89, 120, 103];
-let Mike = [89, 120, 103];
+//case - zwróć średnią z wyników Johna i Mike'a, powiedz która osoba ma średnio lepszy wynik
 
-for(let i = 0; i < John.length; i++){
-    sum += parseInt(John[i],3);
+//case2 - dodać Mary
+
+let John = [89, 120, 103];
+let Mike = [116, 94, 123];
+let Mary = [97, 134, 105];
+
+let sumJ = John.reduce((previous, current) => current += previous); // użycie metody z reduce
+let avgJ = sumJ / John.length;
+
+let total=0; // użycie for in
+for(let i in Mike) { 
+    total += Mike[i]; 
+    avgM = total/Mike.length;
 }
 
-document.write(sum);
+let total2=0; // użycie for in
+for(let x in Mary) { 
+    total2 += Mary[x]; 
+    avgMr = total2/Mary.length;
+}
+
+if (avgJ>avgM && avgJ>avgMr) {
+    document.write('John has best average score');
+}  
+else if (avgJ<avgM && avgM>avgMr) {
+    document.write('Mike has best average score');
+}  
+else if (avgJ<avgMr && avgM<avgMr) {
+    document.write('Mary has best average score');
+}   
+
+document.write('<br><br>John '+avgJ+'<br>Mike '+avgM+'<br>Mary '+avgMr);
+
 
 
